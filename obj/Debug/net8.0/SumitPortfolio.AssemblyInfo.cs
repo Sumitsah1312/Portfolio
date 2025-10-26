eeddb8c592bc6fb8a330464237b15246b052fd6f
sumit shah <sumit.shah@mashvirtual.in>
2025-10-26 20:01:40 +0530

    favicon

--- obj/Debug/net8.0/SumitPortfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/SumitPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SumitPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d27af6cb97ca881772d96dc573f97f79be172f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08ac2d3715edd62ea0b2f849b7d99ff49851857")]
 [assembly: System.Reflection.AssemblyProductAttribute("SumitPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SumitPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
